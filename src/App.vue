cbcaeb138ef2db685550bc25f95a6a44416d92f0
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-07-03 11:11:55 -0300

    Inicializando o projeto controle financeiro

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <button class="btn btn-primary">Botao</button>
+    <router-view />
   </div>
 </template>
 
